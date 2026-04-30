90db8d0fa7d82e4cdb99d4ef66b7d6714d2adfff
smagdi <277685262+smagdi_ejadasa@users.noreply.github.com>
2026-04-30 12:38:43 +0300

    initial commit for loans microservice

--- loans/src/main/java/com/microservice/loans/LoansApplication.java
+++ loans/src/main/java/com/microservice/loans/LoansApplication.java
@@ -1,13 +1,36 @@
 package com.microservice.loans;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "Bank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "salma"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Loans microservice REST API Documentation"
+		)
+)
 public class LoansApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoansApplication.class, args);
 	}
-
 }
